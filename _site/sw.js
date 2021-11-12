61895a8bd0568aeee3b18a098c0de9b474e6562e
docurdt <kurdt325@gmail.com>
2021-11-12 19:41:21 +1100

    add google scholar link

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211112.1925';
+const cacheName = 'chirpy-20211112.1936';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
