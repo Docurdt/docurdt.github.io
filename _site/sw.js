c049bf5072e2ad1d41866af8b0260d00526a16b7
docurdt <kurdt325@gmail.com>
2021-11-12 13:51:33 +1100

    bug fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211112.1330';
+const cacheName = 'chirpy-20211112.1351';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
