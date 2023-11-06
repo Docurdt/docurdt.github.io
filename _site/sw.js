b9ace9f2e18440048726bc8c8486bb42453ccca7
Docurdt <kurdt325@gmail.com>
2023-11-06 16:59:33 +0800

    publication updated

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211119.1529';
+const cacheName = 'chirpy-20211214.2049';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
